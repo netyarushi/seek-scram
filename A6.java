3bbef781218191675b35968d04d8d033a9fd71bc
netyarushi <77297719+netyarushi@users.noreply.github.com>
2021-01-11 14:59:21 -0800

    updated A6 with my code, not gries'

--- A6.java
+++ A6.java
@@ -1,14 +1,6 @@
 
 package diver;
 
-/* NetId(s):
-
- * Name(s):
- * What I thought about this assignment:
- *
- *
- */
-
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -16,100 +8,70 @@ import java.util.List;
 import graph.Edge;
 import graph.Node;
 
+/**
+* Author: Arushi Nety
+* methods written by Arushi: shortestPath(Node, Node), pathSum(List<Node>), coinSum(List<Node>)
+
 /** This class contains the solution to A6, shortest-path algorithm, <br>
- * and other methods for an undirected graph. */
+ * and other methods needed for diver. */
 public class A6 {
-
-    /** Replace "-1" by the time you spent on A2 in hours.<br>
-     * Example: for 3 hours 15 minutes, use 3.25<br>
-     * Example: for 4 hours 30 minutes, use 4.50<br>
-     * Example: for 5 hours, use 5 or 5.0 */
-    // public static double timeSpent= -1;
-
+    
     /** Return the shortest path from node v to node last <br>
      * ---or the empty list if a path does not exist. <br>
-     * Note: The empty list is a list with 0 elements ---it is not "null". */
+     * Note: The empty list is a list with 0 elements. */
     public static List<Node> shortestPath(Node v, Node last) {
-        /* TODO Implement this method.
-         * Read the A6 assignment handout for all details.
-         * Remember, the graph is undirected.
-         * Finally, you will need to declare a HashMap. See the handout for details */
-
         // Contains an entry for each node in the frontier set. The priority of a node
         // is the length of the shortest known path from v to the node using only settled
         // node except for the last node, which is in F
         Heap<Node> F= new Heap<>(true);
-        F.add(v, 0);
+        F.add(v, 0); // adds first node to frontier set; F = {v}; maintains invariant
 
-        // The keys in map mapSF are the nodes in the settled set S and frontier set F.
-        // The values are each node's NodeData, which contains information
-        // such as its distance and its backpointer.
-        HashMap<Node, NodeData> mapSF= new HashMap<>();
-        mapSF.put(v, new NodeData(null, 0));
+        // contains both settled nodes and frontier nodes
+        HashMap<Node, NodeData> SandF= new HashMap<>();
+        NodeData vd= new NodeData(null, 0);
 
-        while (F.size() > 0) {
+        SandF.put(v, vd);
+
+        while (F.size != 0) {
+            // f = node in F with minimum d value
             Node f= F.poll();
-            if (f == last) return path(mapSF, last);
-            int d= mapSF.get(f).dist;
-            for (Edge edge : f.getExits()) {
-                Node w= edge.getOther(f);
-                NodeData wData= mapSF.get(w);
-                int wDist= d + edge.length;
-                if (wData == null) {
-                    mapSF.put(w, new NodeData(f, wDist));
-                    F.add(w, wDist);
-                } else if (wDist < wData.dist) {
-                    wData.dist= wDist;
-                    wData.bkptr= f;
-                    F.changePriority(w, wDist);
+            // return once last node is reached
+            if (f == last) { return path(SandF, last); }
+
+            int fDist= SandF.get(f).dist;
+
+            for (Edge ed : f.getExits()) { // each neighbor of w of f
+                Node w= ed.getOther(f);
+                int len= fDist + ed.length; // distance from starting node -> f -> w
+                NodeData wnd= SandF.get(w);
+
+                if (wnd == null) {
+                    // add w to F
+                    F.add(w, len);
+                    SandF.put(w, new NodeData(f, len));
+
+                } else if (len < wnd.dist) {
+                    wnd.bkptr= f;
+                    wnd.dist= len; // set to smallest of the two; in this case, that's 'len'
+
+                    // change priority to update heap
+                    F.changePriority(w, len);
                 }
+
             }
+
         }
         // no path from v to last
         return new LinkedList<>();
     }
 
-    /** Returns the hash map of all the backpointers of the shortest path
-     *
-     * @param v
-     * @param last
-     * @return */
-    public static HashMap<Node, NodeData> findPath(Node v, Node last) {
-        Heap<Node> F= new Heap<>(true);
-        F.add(v, 0);
-
-        // The keys in map mapSF are the nodes in the settled set S and frontier set F.
-        // The values are each node's NodeData, which contains information
-        // such as its distance and its backpointer.
-        HashMap<Node, NodeData> mapSF= new HashMap<>();
-        mapSF.put(v, new NodeData(null, 0));
-
-        while (F.size() > 0) {
-            Node f= F.poll();
-            if (f == last) return mapSF;
-            int d= mapSF.get(f).dist;
-            for (Edge edge : f.getExits()) {
-                Node w= edge.getOther(f);
-                NodeData wData= mapSF.get(w);
-                int wDist= d + edge.length;
-                if (wData == null) {
-                    mapSF.put(w, new NodeData(f, wDist));
-                    F.add(w, wDist);
-                } else if (wDist < wData.dist) {
-                    wData.dist= wDist;
-                    wData.bkptr= f;
-                    F.changePriority(w, wDist);
-                }
-            }
-        }
-        // no path from v to last
-        return new HashMap<>();
-    }
-
+   
     /** An instance contains information about a node: <br>
      * the Distance of this node from the start node and <br>
      * its Backpointer: the previous node on a shortest path <br>
-     * from the first node to this node (null for the start node). */
+     * from the first node to this node (null for the start node). 
+     * (Not written by Arushi)
+     */
     public static class NodeData {
         /** shortest known distance from the start node to this one. */
         private int dist;
@@ -147,7 +109,8 @@ public class A6 {
 
     /** Return the sum of the weights of the edges on path p. <br>
      * Precondition: pa contains at least 1 node. <br>
-     * If 1 node, it's a path of length 0, i.e. with no edges. */
+     * If 1 node, it's a path of length 0, i.e. with no edges.
+     */
     public static int pathSum(List<Node> p) {
         synchronized (p) {
             Node w= null;
@@ -163,6 +126,9 @@ public class A6 {
         }
     }
 
+    /**
+    * Return the sum of the coins along the path 'p'. <br>
+    */
     public static int coinSum(List<Node> p) {
         int sum= 0;
         for (Node n : p) {
